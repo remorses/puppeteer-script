6ef6a93373a8962dd5dd1f6e9649f3d00550eed3
morse <beats.by.morse@gmail.com>
2018-11-16 17:31:17 +0100

    added metrics in tests

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,7 @@ const {
   fromYAML
 } = require("../lib");
 const path = require("path");
+const cpuStat = require("cpu-stat")
 
 // console.log("current dir:", __dirname);
 // console.log("root dir:", path.resolve( "." ));
@@ -16,7 +17,22 @@ fromYAML("./example.yaml")
     x => {
       const used = process.memoryUsage();
       for (let key in used) {
+        console.log(`memory usge: `)
         console.log(`${key} ${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`);
       }
     }
   )
+
+
+
+setInterval(() => {
+  cpuStat.usagePercent(function (err, percent, seconds) {
+    if (err) {
+      return console.error(err);
+    }
+
+    //the percentage cpu usage over all cores
+    console.log(`using ${Math.round((percent * 100) / 100)}% of cpu`);
+
+  })
+}, 2000)
