5cdf50f802b169f6fa9abfc5a37565dba03997bc
remorses <beats.by.morse@gmail.com>
2018-12-31 21:45:08 +0100

    solved errors

--- tests/index.test.js
+++ tests/index.test.js
@@ -8,7 +8,7 @@ const run = async (callback = data => None) => {
   const browser = await puppeteer.launch({ executablePath: CHROMIUM})
   const page = await browser.newPage()
 
-  const script = Script({ file: './example.yaml', data: { url: 'facebook.com'} })
+  const script = Script({ file: './example.yaml', data: { url: 'https://facebook.com'} })
 
   const data = await execute(script, page)
 
