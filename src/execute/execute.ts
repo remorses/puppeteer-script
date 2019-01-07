ab951cf36c883f0cd18e7226d116eab4e4914aa1
remorses <beats.by.morse@gmail.com>
2019-01-07 18:54:00 +0100

    added use possibility

--- src/execute/execute.ts
+++ src/execute/execute.ts
@@ -9,49 +9,69 @@ const { red, bold, bgRed, white } = chalk
 const { DEBUG = "" } = process.env
 // import { roughSizeOfObject } from "./helpers"
 
+let local = { reducer }
+
 export const execute = (script: Object, page, ) => new Promise((res, rej) => {
 
-  // TODO add plugins when 'use' in script
-  // TODO 
+    // TODO add plugins when 'use' in script
+    // TODO
+    if (script['use']) {
+        script['use']
+            .map(obj => {
+                const key = Object.keys(obj)[0]
+                const value = obj[key]
+                return { method: key, arg: value }
+            })
+            .map(({ method: pkg, arg: settings }) => {
+                import(pkg)
+                    .then(({default: custom_reducer}) => {
+                        local.reducer = pipe(custom_reducer(settings), local.reducer)
+                    })
+            })
+    }
 
-  const actions = script["do"]
-    .map(obj => {
-      const key = Object.keys(obj)[0]
-      const value = obj[key]
-      return { method: key, arg: value }
-    })
+    const actions = script["do"]
+        .map(obj => {
+            const key = Object.keys(obj)[0]
+            const value = obj[key]
+            return { method: key, arg: value }
+        })
 
-  actions.reduce(wrapper, Promise.resolve({ page, data: {} }))
-    .then(x => {
-      logger(bold("done"))
-      return x
-    })
-    .then(state => state.data)
-    .then(res)
-    .catch(rej)
-  })
+    actions.reduce(wrapper, Promise.resolve({ page, data: {} }))
+        .then(x => {
+            logger(bold("done"))
+            return x
+        })
+        .then(state => state.data)
+        .then(res)
+        .catch(rej)
+})
 
 
 
 const wrapper = async (state, action) => {
 
-  return await reducer(await state, action)
+    return await local.reducer(await state, action)
 
-    .then(x => {
-      logExecuted(action )
-      return x
-    })
+        .then(x => {
+            logExecuted(action)
+            return x
+        })
 
-    .catch(e => {
-      logError(e, action)
-      throw new e
-    })
+        .catch(e => {
+            logError(e, action)
+            throw new e
+        })
 
 }
 
 
 
 
+const pipe = (...functions) => input =>
+  functions.reduce(
+    async (promise, func) => func(await promise),
+    Promise.resolve(input))
 
 
 
